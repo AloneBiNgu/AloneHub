9103f42b6ec354c45b7eb0a9c6e5205277692ded
Alone Chúa <90182982+AlongNgu@users.noreply.github.com>
2023-01-20 13:47:10 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,3 @@
 local s, e = pcall(function()
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/AlongNgu/AloneHub/main/%s.lua"):format(tostring(game.PlaceId))))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/AlongNgu/AloneHub/main/%s.lua"):format(tostring(game.GameId))))()
 end)
