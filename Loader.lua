cb57662ba203d7a9c222aee5dc1681977b19ba17
Alone Chúa <90182982+AlongNgu@users.noreply.github.com>
2023-01-20 13:30:29 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,3 @@
 local s, e = pcall(function()
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/AlongNgu/AloneHub/main/%s.lua"):format(tostring(game.PlaceId))))()
 end)
-
-if s == false then game.Players.LocalPlayer:Kick("This game is not supported") end
